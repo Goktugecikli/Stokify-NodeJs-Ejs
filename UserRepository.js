a3ac6b129e7c4db72c322766602d88717514e22b
SKY <onthsky@gmail.com>
2024-06-20 22:17:23 +0300

    company join has added

--- UserRepository.js
+++ UserRepository.js
@@ -1,64 +1,99 @@
-import DatabaseManager from './DbManager.js';
+import DatabaseManager from "./DbManager.js";
 
 class UserRepository {
-    constructor(dbConfig) {
-        this.db = new DatabaseManager(dbConfig);
-    }
+  constructor(dbConfig) {
+    this.dbManager = new DatabaseManager(dbConfig);
+  }
 
-    async GetUserByNameAndPassword(username, password) {
-        try {
-            await this.db.connectDB(); // Veritabanına bağlan
-            let params = { username: username, password: password };
-            let query = "SELECT COUNT(*) AS count FROM Users WHERE username = @username AND password = @password";
-            let result = await this.db.queryWithResult(query, params);
-            return result[0].count;
-        } catch (err) {
-            console.error('Error repossitory connection. Err ', err);
-        } finally {
-            await this.db.closeDB(); // Veritabanı bağlantısını kapat
-        }
+  async GetUserDetailsByUserName(userName) {
+    try {
+      await this.dbManager.connectDB();
+      let params = { userName: userName };
+      let query = "SELECT * FROM Users WHERE UserName = @userName";
+      return await this.dbManager.queryWithResult(query, params);
+    } catch (err) {
+      console.error("Error retrieving user details:", err);
+    } finally {
+      await this.dbManager.closeDB();
     }
+  }
 
-    async getUserById(username) {
-        try {
-            await this.db.connectDB();
-            let params = { username: username };
-            let query = "SELECT * FROM Users WHERE Username = @username";
-            let result = await this.db.queryWithResult(query, params);
-            // console.log(result);
-            // console.log(result[0].Password);
-            // console.log(result[0].Email);
-            // console.log(result[0].UserId);
-            return result; // Tek bir kullanıcı döneceği varsayımıyla
-        } catch (err) {
-            console.error('Error repository connection. Err', err);
-        } finally {
-            await this.db.closeDB();
-        }
+  async JoinCompanyByUserIdAndCompanyId(userId, companyId) {
+    try {
+      await this.dbManager.connectDB();
+      let params = { userId: userId, companyId: companyId };
+      let query = "INSERT INTO CompanyUsers (UserId, CompanyId) VALUES (@userId, @companyId);SELECT SCOPE_IDENTITY() AS Id;";
+      return await this.dbManager.queryWithResult(query, params);
+    } catch (err) {
+      console.error("Error joining company:", err);
+    } finally {
+      await this.dbManager.closeDB();
     }
+  }
 
-    async Register(firstName, lastName, email, userName, password) {
-        let params =
-        {
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            userName: userName,
-            password: password
-        }
-        try {
-            await this.db.connectDB();
-
-            let query = "INSERT INTO Users (Username,Password,Firstname,LastName,Email) VALUES (@userName,@password,@firstName,@lastName,@email)";
-            let result = await this.db.queryWithoutResult(query, params);
-            return result;
-        } catch (error) {
-            console.log("register error : "+ error);
-        }
-        finally {
-            await this.db.closeDB(); // Veritabanı bağlantısını kapat
-        }
+  async GetUserByNameAndPassword(username, password) {
+    try {
+      await this.dbManager.connectDB();
+      let params = { username: username, password: password };
+      let query = "SELECT COUNT(*) AS count FROM Users WHERE Username = @username AND Password = @password";
+      let result = await this.dbManager.queryWithResult(query, params);
+      return result[0].count;
+    } catch (err) {
+      console.error("Error retrieving user by name and password:", err);
+    } finally {
+      await this.dbManager.closeDB();
     }
+  }
+
+  async GetCompanyByUserId(userId) {
+    try {
+      await this.dbManager.connectDB();
+      let params = { userId: userId };
+      let query = "SELECT CompanyId FROM CompanyUsers WHERE UserId = CONVERT(uniqueidentifier, @userId)";
+      return await this.dbManager.queryWithResult(query, params);
+    } catch (err) {
+      console.error("Error retrieving company by user ID:", err);
+    } finally {
+      await this.dbManager.closeDB();
+    }
+  }
+
+  async Register(firstName, lastName, email, userName, password) {
+    let params = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      userName: userName,
+      password: password,
+    };
+    try {
+      await this.dbManager.connectDB();
+      let query = "INSERT INTO Users (Username, Password, Firstname, LastName, Email) VALUES (@userName, @password, @firstName, @lastName, @email)";
+      await this.dbManager.queryWithoutResult(query, params);
+    } catch (err) {
+      console.error("Error registering user:", err);
+    } finally {
+      await this.dbManager.closeDB();
+    }
+  }
+
+  async getUserById(username) {
+    try {
+      await this.dbManager.connectDB();
+      let params = { username: username };
+      let query = "SELECT * FROM Users WHERE Username = @username";
+      let result = await this.dbManager.queryWithResult(query, params);
+      // console.log(result);
+      // console.log(result[0].Password);
+      // console.log(result[0].Email);
+      // console.log(result[0].UserId);
+      return result; // Tek bir kullanıcı döneceği varsayımıyla
+    } catch (err) {
+      console.error("Error repository connection. Err", err);
+    } finally {
+      await this.dbManager.closeDB();
+    }
+  }
 
 }
 
--- UserService.js
+++ UserService.js
@@ -1,44 +1,95 @@
 import UserRepository from "./UserRepository.js";
-
- 
+import CompanyService from "./CompanyService.js";
 class UserService {
-    constructor() {
-        this.userRepository = new UserRepository();
-    }
+  constructor() {
+    this.userRepository = new UserRepository();
+    this.companyService = new CompanyService();
+  }
 
-     async validateUser(username, password) {
-        try {
-            var result = await this.userRepository.GetUserByNameAndPassword(username, password);
-            return result;
-        } catch (err) {
-            console.error('Error validating user', err);
-        } 
+  async validateUser(username, password) {
+    try {
+      var result = await this.userRepository.GetUserByNameAndPassword(
+        username,
+        password
+      );
+      return result;
+    } catch (err) {
+      console.error("Error validating user", err);
     }
+  }
 
-    async IsExistUser(username){
-        try {
-            var result = await this.userRepository.getUserById(username);
-            return result;
-        } catch (err) {
-            console.error('Error validating user', err);
-        }
+  async IsExistUser(username) {
+    try {
+      var result = await this.userRepository.getUserById(username);
+      return result;
+    } catch (err) {
+      console.error("Error validating user", err);
     }
+  }
 
-    async Register(firstName,lastName,email,userName,password){
-        try { 
-            
-            var result = await this.userRepository.Register(firstName, lastName, email, userName, password);
-            if(result === 0){
-                return false;
-            }
-            return true;
-        } catch (err) {
-            console.error('Error registering user :  ' + err);
-            return false;
-        }
+  async JoinCompanyByInvateCode(invateCode, userName) {
+    try {
+      let companyDetailsResult =
+        await this.companyService.GetCompanyDetailsByInvateCode(invateCode);
+      if (!companyDetailsResult || companyDetailsResult.length === 0) {
+        return { success: false, message: "Wrong InvateCode" };
+      }
+      let userDetailsResult = await this.GetUserDetailsByUserName(userName);
+      if (!userDetailsResult || userDetailsResult.length === 0) {
+        return { success: false, message: "Error Get UserDetails" };
+      }
+      let joinResult =
+        await this.userRepository.JoinCompanyByUserIdAndCompanyId(
+          userDetailsResult[0].UserId,
+          companyDetailsResult[0].CompanyId
+        );
+      if (!joinResult || joinResult.length === 0) {
+        return { success: false, message: "Error Join Company" };
+      }
+      return ({success:true})
+    } catch (err) {}
+  }
+
+  async GetUserDetailsByUserName(userName) {
+    try {
+      return this.userRepository.GetUserDetailsByUserName(userName);
+    } catch (err) {}
+  }
+
+  async GetCompanyByUserId(userId) {
+    try {
+      var result = await this.userRepository.GetCompanyByUserId(userId);
+      if (!result || result.length === 0) {
+        return null;
+      }
+      var companyDetails = await this.companyService.GetCompanyDetailsById(
+        result[0].CompanyId
+      );
+      return companyDetails;
+    } catch (err) {
+      console.error("Error copmany find", err);
     }
- 
-    // Diğer metodları buraya ekleyin...
+  }
+  async Register(firstName, lastName, email, userName, password) {
+    try {
+      var result = await this.userRepository.Register(
+        firstName,
+        lastName,
+        email,
+        userName,
+        password
+      );
+      if (result === 0) {
+        return false;
+      }
+      return true;
+    } catch (err) {
+      console.error("Error registering user :  " + err);
+      return false;
+    }
+  }
+
+  // Diğer metodları buraya ekleyin...
 }
 
 export default UserService;
