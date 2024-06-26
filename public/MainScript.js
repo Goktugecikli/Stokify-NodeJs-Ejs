1738e86a653db8bd94627c9d1b3df9069f2f047d
goktugecikli <ggecikli@hotmail.com>
2024-06-26 03:00:29 +0300

    home page design update

--- public/MainScript.js
+++ public/MainScript.js
@@ -9,3 +9,20 @@ function togglePassword() {
         toggleButton.textContent = "Show";
     }
 }
+
+let i = 0;
+
+function PrintTitles() {
+
+    var titles = ["Web Designer", "Software Developer", "Game Developer", "Graphic Designer"];
+
+
+    document.getElementById("titles").innerHTML = titles[i];
+    i++;
+    if (i >= titles.length) {
+        i = 0;
+    }
+}
+
+var btnPrint = document.getElementById("title_change");
+btnPrint.onclick = PrintTitles;
